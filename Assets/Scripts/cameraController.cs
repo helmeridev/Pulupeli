9b5d24bfdcc0f7ecfaaf5c6b9b15b65bc0dd718a
helmeridev <s2300886@edu.bc.fi>
2025-08-22 09:52:56 +0300

    Update
    +added cameracontroller and redid the pulu sprite

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
